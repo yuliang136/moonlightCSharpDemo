31d7ca21ed68712f6e3c4c0618051bd1f7d24a16
yuliang <yuliang@naocy.com>
2016-04-22 10:45:03 +0800

    获得当前系统时间函数
    获得当前系统时间函数

---
+++ GetCurrentTimeString/GetCurrentTimeString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetCurrentTimeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strTime = DateTime.Now.ToString("yyyyMMdd-hhmmss");
+
+            Console.WriteLine(strTime);
+        }
+    }
+}
